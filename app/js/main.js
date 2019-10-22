663593efeaf0b4cd8e3ee891ba210c5f8eba9c56
St4y-23 <ghost19011@gmail.com>
2019-10-22 07:17:55 +0400

    added more and tabs

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,44 @@ $(function(){
   $('.slider-top__slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false,
+    arrows: true,
+    centerMode: true,
+    prevArrow: '<button class="slick-arrow slick-prev"></button>',
+    nextArrow: '<button class="slick-arrow slick-next"></button>',
   });
+
+  $('.slider__inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    prevArrow: '<button class="slick-arrow slick-prev"></button>',
+    nextArrow: '<button class="slick-arrow slick-next"></button>',
+  });
+
+
+  
+    $('ul.tabs').delegate('li:not(.current)', 'click', function () {
+        act(this);
+    })
+    $('.prev').click(function(){
+        var i=$('.current').index();
+        var obj=$('.tabs > li');
+        i=(i<1)? obj.length-1: i-1;
+        act(obj.get(i));
+    });
+    $('.next').click(function(){
+        var i=$('.current').index();
+        var obj=$('.tabs > li');
+        i=++i%obj.length;
+        act(obj.get(i));
+    });
+    function act(Obj){
+        $(Obj).addClass('current').siblings().removeClass('current')
+            .parents('div.fashion__inner').find('div.box').eq($(Obj).index())
+            .show().siblings('div.box').hide();
+    };
+
 
 
   
