a42da56befaaa1ed614be7c336183d87543c7c3b
St4y-23 <ghost19011@gmail.com>
2019-10-23 07:20:13 +0400

    added fashion, logotype and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
   $('.slider-top__slider').slick({
     slidesToShow: 1,
@@ -19,29 +19,37 @@ $(function(){
   });
 
 
-  
-    $('ul.tabs').delegate('li:not(.current)', 'click', function () {
-        act(this);
-    })
-    $('.prev').click(function(){
-        var i=$('.current').index();
-        var obj=$('.tabs > li');
-        i=(i<1)? obj.length-1: i-1;
-        act(obj.get(i));
-    });
-    $('.next').click(function(){
-        var i=$('.current').index();
-        var obj=$('.tabs > li');
-        i=++i%obj.length;
-        act(obj.get(i));
-    });
-    function act(Obj){
-        $(Obj).addClass('current').siblings().removeClass('current')
-            .parents('div.fashion__inner').find('div.box').eq($(Obj).index())
-            .show().siblings('div.box').hide();
-    };
+
+  $('ul.tabs').delegate('li:not(.current)', 'click', function () {
+    act(this);
+  })
+  $('.prev').click(function () {
+    var i = $('.current').index();
+    var obj = $('.tabs > li');
+    i = (i < 1) ? obj.length - 1 : i - 1;
+    act(obj.get(i));
+  });
+  $('.next').click(function () {
+    var i = $('.current').index();
+    var obj = $('.tabs > li');
+    i = ++i % obj.length;
+    act(obj.get(i));
+  });
+  function act(Obj) {
+    $(Obj).addClass('current').siblings().removeClass('current')
+      .parents('div.fashion__inner').find('div.box').eq($(Obj).index())
+      .show().siblings('div.box').hide();
+  };
+
+
+  $('.logotype__inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slick-arrow slick-prev"></button>',
+    nextArrow: '<button class="slick-arrow slick-next"></button>',
+  });
 
 
 
-  
+
 });
